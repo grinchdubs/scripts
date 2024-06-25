993ca907cd67be714ea6f9a0d3c8760a593c8fd8
GRNCH <cubanb95@gmail.com>
2024-06-25 13:11:14 -0400

    updated commit line

--- chezmoiUpdater.sh
+++ chezmoiUpdater.sh
@@ -4,8 +4,9 @@
 chezmoi diff #diff to see chganges
 read -p "Press any key..." #allowing use to look at the changes to proceed
 chezmoi apply #apply the changes to chezmoi
+cd /home/grnch/.local/share/chezmoi #moving to chezmoi dir
 git add . #adding the dir to git, not sure if this is needed
-read commitin #getting a commit from user
+read -p 'Git Commit: ' commitin #getting a commit from user
 git commit -m "$commitin" #applying the commit
 git push -u origin main && echo "Dotfiles Updated!" || echo "Push Failed!" #pushing to the main branch and telling the user the script has run successfully or failed
 
