80e8b8ade18d17e2fb83568b409507947082265b
GRNCH <cubanb95@gmail.com>
2024-07-06 16:25:59 -0400

    Update base_installs.sh

--- base_installs.sh
+++ base_installs.sh
@@ -11,6 +11,6 @@ fish_add_path /home/grnch/.cargo/bin
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install PatrickF1/fzf.fish
 wget https://raw.githubusercontent.com/grinchdubs/dotfiles/main/private_dot_config/private_fish/config.fish
-mv config.fish ./config/fish/config.fish
+mv config.fish .config/fish/config.fish
 sudo snap install localsend
 wget https://gist.githubusercontent.com/joaopizani/2718397/raw/9e2560b77e1e1298ef24be16297d853f9885b20d/.screenrc
